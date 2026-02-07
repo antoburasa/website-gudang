5286b553d98dc0d8c99628de9d45a734f4a9aaa7
antoburasa <Adrianto_st@yahoo.co.id>
2026-02-07 14:05:56 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-// ===== LOGIN =====
+console.log("app.js TERLOAD");
+
+// LOGIN
 function login(){
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
 
-  // username & password sederhana
   if(user === "admin" && pass === "123"){
     localStorage.setItem("login", "1");
-    window.location.href = "dashboard.html";
-  }else{
+    window.location.href = "./dashboard.html";
+  } else {
     alert("Username atau password salah");
   }
 }
 
-// ===== CEK LOGIN DI DASHBOARD =====
-if (document.title === "Dashboard") {
+// CEK LOGIN DI DASHBOARD
+if (document.body.innerHTML.includes("Dashboard Gudang")) {
   if (localStorage.getItem("login") !== "1") {
-    window.location.href = "index.html";
+    window.location.href = "./index.html";
   }
 }
 
-// ===== LOGOUT =====
+// LOGOUT
 function logout(){
   localStorage.removeItem("login");
-  window.location.href = "index.html";
+  window.location.href = "./index.html";
 }
