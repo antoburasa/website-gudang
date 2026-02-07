a939fc0a9f31e5c8d9eb9bee3976beaa3bcf1a2a
antoburasa <Adrianto_st@yahoo.co.id>
2026-02-07 13:53:05 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,79 @@
-// ===== LOGIN =====
-function login() {
-  const u = document.getElementById("user").value;
-  const p = document.getElementById("pass").value;
-
-  if (u === "admin" && p === "1234") {
-    localStorage.setItem("login", "ok");
-    window.location.href = "dashboard.html";
-  } else {
-    document.getElementById("msg").innerText = "Username / Password salah";
-  }
-}
-
-// ===== LOGOUT =====
-function logout() {
-  localStorage.removeItem("login");
-  window.location.href = "index.html";
-}
-
-// ===== PROTEKSI DASHBOARD =====
+// ===== CEK LOGIN =====
 if (document.title === "Dashboard") {
-  if (localStorage.getItem("login") !== "ok") {
+  if (localStorage.getItem("login") !== "1") {
     window.location.href = "login.html";
   }
+}
+
+// ===== MENU =====
+function showMenu(id){
+  document.querySelectorAll(".card").forEach(c=>{
+    c.classList.remove("active");
+  });
+  document.getElementById(id).classList.add("active");
+}
+
+// ===== DATA =====
+let data = JSON.parse(localStorage.getItem("barang")) || {};
+
+// ===== MASTER BARANG =====
+function simpanBarang(){
+  const nama = document.getElementById("namaBarang").value;
+  if(nama === ""){
+    alert("Nama barang kosong");
+    return;
+  }
+  if(!data[nama]){
+    data[nama] = 0;
+    localStorage.setItem("barang", JSON.stringify(data));
+    alert("Barang disimpan");
+    document.getElementById("namaBarang").value = "";
+    tampilStok();
+  }else{
+    alert("Barang sudah ada");
+  }
+}
+
+// ===== BARANG MASUK =====
+function barangMasuk(){
+  const nama = document.getElementById("barangMasuk").value;
+  const qty = Number(document.getElementById("qtyMasuk").value);
+  if(!data[nama]){
+    alert("Barang belum ada");
+    return;
+  }
+  data[nama] += qty;
+  localStorage.setItem("barang", JSON.stringify(data));
+  alert("Barang masuk berhasil");
+  tampilStok();
+}
+
+// ===== BARANG KELUAR =====
+function barangKeluar(){
+  const nama = document.getElementById("barangKeluar").value;
+  const qty = Number(document.getElementById("qtyKeluar").value);
+  if(!data[nama]){
+    alert("Barang belum ada");
+    return;
+  }
+  data[nama] -= qty;
+  localStorage.setItem("barang", JSON.stringify(data));
+  alert("Barang keluar berhasil");
+  tampilStok();
+}
+
+// ===== STOK =====
+function tampilStok(){
+  let html = "";
+  for(let b in data){
+    html += <div>${b} : ${data[b]}</div>;
+  }
+  document.getElementById("listStok").innerHTML = html || "Belum ada data";
+}
+tampilStok();
+
+// ===== LOGOUT =====
+function logout(){
+  localStorage.removeItem("login");
+  window.location.href = "index.html";
 }
